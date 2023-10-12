9f04e51ba29c85217303f734b3cf4e2fffaa704a
taral.bytes <taral.nagar@bytestechnolab>
2023-10-12 11:38:49 +0530

    Modified config file

--- src/config/custom_exception_handler.php
+++ src/config/custom_exception_handler.php
@@ -1,198 +1,305 @@
 <?php
 
 return [
-    'exceptions' => [
-        [
-            'exception_class_name' => 'Exception',
-            'custom_response' => [
-                'error' => 'Exception',
-                'message' => 'An exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'Error',
-            'custom_response' => [
-                'error' => 'Error',
-                'message' => 'An error occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'RuntimeException',
-            'custom_response' => [
-                'error' => 'RuntimeException',
-                'message' => 'A runtime exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'InvalidArgumentException',
-            'custom_response' => [
-                'error' => 'InvalidArgumentException',
-                'message' => 'Invalid argument provided.',
-                'hi' => 'b'
-            ],
-        ],
-        [
-            'exception_class_name' => 'LogicException',
-            'custom_response' => [
-                'error' => 'LogicException',
-                'message' => 'A logic exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'LengthException',
-            'custom_response' => [
-                'error' => 'LengthException',
-                'message' => 'A length exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'OutOfBoundsException',
-            'custom_response' => [
-                'error' => 'OutOfBoundsException',
-                'message' => 'An out-of-bounds exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'RangeException',
-            'custom_response' => [
-                'error' => 'RangeException',
-                'message' => 'A range exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'DomainException',
-            'custom_response' => [
-                'error' => 'DomainException',
-                'message' => 'A domain exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'UnderflowException',
-            'custom_response' => [
-                'error' => 'UnderflowException',
-                'message' => 'An underflow exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'OverflowException',
-            'custom_response' => [
-                'error' => 'OverflowException',
-                'message' => 'An overflow exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'BadFunctionCallException',
-            'custom_response' => [
-                'error' => 'BadFunctionCallException',
-                'message' => 'A bad function call exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'BadMethodCallException',
-            'custom_response' => [
-                'error' => 'BadMethodCallException',
-                'message' => 'A bad method call exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'OutOfBoundsException',
-            'custom_response' => [
-                'error' => 'OutOfBoundsException',
-                'message' => 'An out-of-bounds exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'UnexpectedValueException',
-            'custom_response' => [
-                'error' => 'UnexpectedValueException',
-                'message' => 'An unexpected value exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'PDOException',
-            'custom_response' => [
-                'error' => 'PDOException',
-                'message' => 'A database exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'HttpRuntimeException',
-            'custom_response' => [
-                'error' => 'HttpRuntimeException',
-                'message' => 'An HTTP runtime exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'HttpResponseException',
-            'custom_response' => [
-                'error' => 'HttpResponseException',
-                'message' => 'An HTTP response exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'ModelNotFoundException',
-            'custom_response' => [
-                'error' => 'ModelNotFoundException',
-                'message' => 'A model not found exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'ValidationException',
-            'custom_response' => [
-                'error' => 'ValidationException',
-                'message' => 'A validation exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'AuthenticationException',
-            'custom_response' => [
-                'error' => 'AuthenticationException',
-                'message' => 'An authentication exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'AuthorizationException',
-            'custom_response' => [
-                'error' => 'AuthorizationException',
-                'message' => 'An authorization exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'HttpException',
-            'custom_response' => [
-                'error' => 'HttpException',
-                'message' => 'An HTTP exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'PostTooLargeException',
-            'custom_response' => [
-                'error' => 'PostTooLargeException',
-                'message' => 'A post is too large exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'TooManyRequestsHttpException',
-            'custom_response' => [
-                'error' => 'TooManyRequestsHttpException',
-                'message' => 'Too many requests exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'MaintenanceModeException',
-            'custom_response' => [
-                'error' => 'MaintenanceModeException',
-                'message' => 'Maintenance mode exception occurred.',
-            ],
-        ],
-        [
-            'exception_class_name' => 'TokenMismatchException',
-            'custom_response' => [
-                'error' => 'TokenMismatchException',
-                'message' => 'Token mismatch exception occurred.',
-            ],
-        ],
-        // Add more exception configurations as needed
-    ],
+    // 'exceptions' => [
+    //     [
+    //         'exception_class_name' => 'Exception',
+    //         'custom_response' => [
+    //             'error' => 'Exception',
+    //             'message' => 'An exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'Error',
+    //         'custom_response' => [
+    //             'error' => 'Error',
+    //             'message' => 'An error occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'RuntimeException',
+    //         'custom_response' => [
+    //             'error' => 'RuntimeException',
+    //             'message' => 'A runtime exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'InvalidArgumentException',
+    //         'custom_response' => [
+    //             'error' => 'InvalidArgumentException',
+    //             'message' => 'Invalid argument provided.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'LogicException',
+    //         'custom_response' => [
+    //             'error' => 'LogicException',
+    //             'message' => 'A logic exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'LengthException',
+    //         'custom_response' => [
+    //             'error' => 'LengthException',
+    //             'message' => 'A length exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'OutOfBoundsException',
+    //         'custom_response' => [
+    //             'error' => 'OutOfBoundsException',
+    //             'message' => 'An out-of-bounds exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'RangeException',
+    //         'custom_response' => [
+    //             'error' => 'RangeException',
+    //             'message' => 'A range exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'DomainException',
+    //         'custom_response' => [
+    //             'error' => 'DomainException',
+    //             'message' => 'A domain exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'UnderflowException',
+    //         'custom_response' => [
+    //             'error' => 'UnderflowException',
+    //             'message' => 'An underflow exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'OverflowException',
+    //         'custom_response' => [
+    //             'error' => 'OverflowException',
+    //             'message' => 'An overflow exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'BadFunctionCallException',
+    //         'custom_response' => [
+    //             'error' => 'BadFunctionCallException',
+    //             'message' => 'A bad function call exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'BadMethodCallException',
+    //         'custom_response' => [
+    //             'error' => 'BadMethodCallException',
+    //             'message' => 'A bad method call exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'OutOfBoundsException',
+    //         'custom_response' => [
+    //             'error' => 'OutOfBoundsException',
+    //             'message' => 'An out-of-bounds exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'UnexpectedValueException',
+    //         'custom_response' => [
+    //             'error' => 'UnexpectedValueException',
+    //             'message' => 'An unexpected value exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'PDOException',
+    //         'custom_response' => [
+    //             'error' => 'PDOException',
+    //             'message' => 'A database exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'HttpRuntimeException',
+    //         'custom_response' => [
+    //             'error' => 'HttpRuntimeException',
+    //             'message' => 'An HTTP runtime exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'HttpResponseException',
+    //         'custom_response' => [
+    //             'error' => 'HttpResponseException',
+    //             'message' => 'An HTTP response exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'ModelNotFoundException',
+    //         'custom_response' => [
+    //             'error' => 'ModelNotFoundException',
+    //             'message' => 'A model not found exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'ValidationException',
+    //         'custom_response' => [
+    //             'error' => 'ValidationException',
+    //             'message' => 'A validation exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'AuthenticationException',
+    //         'custom_response' => [
+    //             'error' => 'AuthenticationException',
+    //             'message' => 'An authentication exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'AuthorizationException',
+    //         'custom_response' => [
+    //             'error' => 'AuthorizationException',
+    //             'message' => 'An authorization exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'HttpException',
+    //         'custom_response' => [
+    //             'error' => 'HttpException',
+    //             'message' => 'An HTTP exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'PostTooLargeException',
+    //         'custom_response' => [
+    //             'error' => 'PostTooLargeException',
+    //             'message' => 'A post is too large exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'TooManyRequestsHttpException',
+    //         'custom_response' => [
+    //             'error' => 'TooManyRequestsHttpException',
+    //             'message' => 'Too many requests exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'MaintenanceModeException',
+    //         'custom_response' => [
+    //             'error' => 'MaintenanceModeException',
+    //             'message' => 'Maintenance mode exception occurred.',
+    //         ],
+    //     ],
+    //     [
+    //         'exception_class_name' => 'TokenMismatchException',
+    //         'custom_response' => [
+    //             'error' => 'TokenMismatchException',
+    //             'message' => 'Token mismatch exception occurred.',
+    //         ],
+    //     ],
+    //     // Add more exception configurations as needed
+    // ],
     // Add more configuration options if required
+
+
+    'exceptions' => [
+        'Exception',
+        'Error',
+        'RuntimeException',
+        'InvalidArgumentException',
+        'LogicException',
+        'LengthException',
+        'OutOfBoundsException',
+        'RangeException',
+        'DomainException',
+        'UnderflowException',
+        'OverflowException',
+        'BadFunctionCallException',
+        'BadMethodCallException',
+        'OutOfBoundsException',
+        'UnexpectedValueException',
+        'PDOException',
+        'HttpRuntimeException',
+        'HttpResponseException',
+        'ModelNotFoundException',
+        'ValidationException',
+        'AuthenticationException',
+        'AuthorizationException',
+        'HttpException',
+        'PostTooLargeException',
+        'TooManyRequestsHttpException',
+        'MaintenanceModeException',
+        'TokenMismatchException',
+        'DivisionByZeroError'
+        // Add more exception class names as needed
+    ],
+
+    'exceptions_messages' => [
+        'Exception' => 'An exception occurred.',
+        'Error' => 'An error occurred.',
+        'RuntimeException' => 'A runtime exception occurred.',
+        'InvalidArgumentException' => 'Invalid argument provided.',
+        'LogicException' => 'A logic exception occurred.',
+        'LengthException' => 'A length exception occurred.',
+        'OutOfBoundsException' => 'An out-of-bounds exception occurred.',
+        'RangeException' => 'A range exception occurred.',
+        'DomainException' => 'A domain exception occurred.',
+        'UnderflowException' => 'An underflow exception occurred.',
+        'OverflowException' => 'An overflow exception occurred.',
+        'BadFunctionCallException' => 'A bad function call exception occurred.',
+        'BadMethodCallException' => 'A bad method call exception occurred.',
+        'OutOfBoundsException' => 'An out-of-bounds exception occurred.',
+        'UnexpectedValueException' => 'An unexpected value exception occurred.',
+        'PDOException' => 'A database exception occurred.',
+        'HttpRuntimeException' => 'An HTTP runtime exception occurred.',
+        'HttpResponseException' => 'An HTTP response exception occurred.',
+        'ModelNotFoundException' => 'A model not found exception occurred.',
+        'ValidationException' => 'A validation exception occurred.',
+        'AuthenticationException' => 'An authentication exception occurred.',
+        'AuthorizationException' => 'An authorization exception occurred.',
+        'HttpException' => 'An HTTP exception occurred.',
+        'PostTooLargeException' => 'A post is too large exception occurred.',
+        'TooManyRequestsHttpException' => 'Too many requests exception occurred.',
+        'MaintenanceModeException' => 'Maintenance mode exception occurred.',
+        'TokenMismatchException' => 'Token mismatch exception occurred.',
+        'DivisionByZeroError' => 'Divide by zero error.'
+        // Add more exception class names and messages as needed
+    ],
+
+    'exceptions_status_codes' => [
+        'Exception' => 500,                     // Internal Server Error
+        'Error' => 500,                         // Internal Server Error
+        'RuntimeException' => 500,              // Internal Server Error
+        'InvalidArgumentException' => 400,     // Bad Request
+        'LogicException' => 500,                // Internal Server Error
+        'LengthException' => 500,               // Internal Server Error
+        'OutOfBoundsException' => 500,          // Internal Server Error
+        'RangeException' => 500,                // Internal Server Error
+        'DomainException' => 500,               // Internal Server Error
+        'UnderflowException' => 500,            // Internal Server Error
+        'OverflowException' => 500,             // Internal Server Error
+        'BadFunctionCallException' => 400,      // Bad Request
+        'BadMethodCallException' => 400,        // Bad Request
+        'OutOfBoundsException' => 500,          // Internal Server Error
+        'UnexpectedValueException' => 500,      // Internal Server Error
+        'PDOException' => 500,                  // Internal Server Error
+        'HttpRuntimeException' => 500,           // Internal Server Error
+        'HttpResponseException' => 500,         // Internal Server Error
+        'ModelNotFoundException' => 404,        // Not Found
+        'ValidationException' => 422,           // Unprocessable Entity
+        'AuthenticationException' => 401,       // Unauthorized
+        'AuthorizationException' => 403,        // Forbidden
+        'HttpException' => 500,                // Internal Server Error
+        'PostTooLargeException' => 413,         // Payload Too Large
+        'TooManyRequestsHttpException' => 429,  // Too Many Requests
+        'MaintenanceModeException' => 503,      // Service Unavailable
+        'TokenMismatchException' => 419,       // Authentication Timeout
+        'DivisionByZeroError' => 500
+        // Add more exception class names and status codes as needed
+    ],
+
+    'custom_data' => [
+
+        // Add custom Data dynamically inside data key
+        // Exception => [
+        //     'custom_key' => ''
+        //     'data' => [
+
+        //     ]
+        // ]
+    ]
 ];
