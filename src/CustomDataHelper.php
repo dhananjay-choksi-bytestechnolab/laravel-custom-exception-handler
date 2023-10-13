718088ee483be11b219fb0d6df7cda917c05fb28
taral.bytes <taral.nagar@bytestechnolab>
2023-10-13 11:52:47 +0530

    Bugfix regarding adding custom data

--- src/CustomDataHelper.php
+++ src/CustomDataHelper.php
@@ -4,27 +4,39 @@ namespace BytesTechnolabs\CustomExceptionHandler;
 
 class CustomDataHelper
 {
+    /**
+     * Add custom data to Laravel configuration based on an exception.
+     *
+     * @param \Exception $exception The exception for which custom data is being added.
+     * @param array $data Additional data to be associated with the exception.
+     * @param string $customKey The key to store the custom data in the configuration.
+     * @return array The generated configuration for reference.
+     */
     public static function addCustomDataToConfig($exception, $data = [], $customKey = '')
     {
+        // Get the default key for exception response data from Laravel configuration.
         $defaultExceptionResponseDataKey = config("custom_exception_handler.default_exception_response_data_key");
+
+        // Determine the key to store the custom data based on input and configuration.
         if (empty($customKey) && !empty($defaultExceptionResponseDataKey)) {
             $customKey = $defaultExceptionResponseDataKey;
-        } else {
+        } elseif (empty($defaultExceptionResponseDataKey)) {
             $customKey = 'data';
         }
-        
-        // Generate the configuration keys based on the exception class name
+
+        // Generate the configuration keys based on the exception class name.
         $configKey = 'custom_exception_handler.custom_data.' . get_class($exception) . '.';
 
-        // Define the configuration values
+        // Define the configuration values to be set.
         $config = [
             $configKey . 'custom_key' => $customKey,
             $configKey . 'data' => $data,
         ];
 
-        // Set the custom data dynamically in the Laravel configuration
+        // Set the custom data dynamically in the Laravel configuration.
         config($config);
 
-        return $config; // Return the generated configuration for reference
+        // Return the generated configuration for reference.
+        return $config;
     }
 }
